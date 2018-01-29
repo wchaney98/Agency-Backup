10053d17720a85b0d1d774a3e8e43fe45e074884
Will Chaney <wills.apps@yahoo.com>
2018-01-29 15:39:17 -0700

    Re-org and some level code

---
+++ Agency/Assets/Resources/Scripts/Contracts/LevelBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType
+{
+    Floor,
+    Wall
+}
+
+public static class LevelBuilder
+{
+    static TileType[][] tiles;
+
+    public static void BuildLevel()
+    {
+
+    }
+}
--- Agency/Assets/Resources/Scripts/Contracts/LevelGenerator.cs
+++ Agency/Assets/Resources/Scripts/Contracts/LevelGenerator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelBuilder : MonoBehaviour
+public class LevelGenerator : MonoBehaviour
 {
 
     void Start()
